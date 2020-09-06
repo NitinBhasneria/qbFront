d9db16860ef15f85eb279bc15f7aafd0304aa49a
NitinBhasneria <nitinbhasneria360@gmail.com>
2020-09-06 11:25:54 +0530

    Updates

---
+++ precache-manifest.2c8328b67ccc10894e51794f1a9b4430.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d494afc675c6748366543bf5acc852",
+    "url": "/qbFront/index.html"
+  },
+  {
+    "revision": "b5c342b57a1f3bdca29e",
+    "url": "/qbFront/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e3cf6c6204998c3a68e9",
+    "url": "/qbFront/static/css/main.8ed9ef08.chunk.css"
+  },
+  {
+    "revision": "b5c342b57a1f3bdca29e",
+    "url": "/qbFront/static/js/2.09820ab5.chunk.js"
+  },
+  {
+    "revision": "7e7427604f2a30a1e5ccb774dda109dd",
+    "url": "/qbFront/static/js/2.09820ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3cf6c6204998c3a68e9",
+    "url": "/qbFront/static/js/main.828e878b.chunk.js"
+  },
+  {
+    "revision": "0d3e5fd6870a2cb3b7d6",
+    "url": "/qbFront/static/js/runtime-main.0321e445.js"
+  },
+  {
+    "revision": "b7736087a0dc40a789f8c4043fcf46e6",
+    "url": "/qbFront/static/media/Accountancy.b7736087.png"
+  },
+  {
+    "revision": "f647926155791e4f430936e9b055889b",
+    "url": "/qbFront/static/media/AccountancyBlue.f6479261.png"
+  },
+  {
+    "revision": "43ef66dbf860a8823c52ab7c64e37a2f",
+    "url": "/qbFront/static/media/AdvanceFilter.43ef66db.png"
+  },
+  {
+    "revision": "3fcf61602c8e3931b9bd05e91e43aa87",
+    "url": "/qbFront/static/media/AdvanceFilterMobile.3fcf6160.png"
+  },
+  {
+    "revision": "0b760423331b7c1f4797b1700a78a908",
+    "url": "/qbFront/static/media/Biology.0b760423.png"
+  },
+  {
+    "revision": "e44a0ecf23ddedfbfdf292300e6adef7",
+    "url": "/qbFront/static/media/BiologyBlue.e44a0ecf.png"
+  },
+  {
+    "revision": "2bd3c34efb983f1e794dcbaea6e55904",
+    "url": "/qbFront/static/media/Bussiness Studies.2bd3c34e.png"
+  },
+  {
+    "revision": "8c53cd3ddc5e988d47e7bcb8f212f751",
+    "url": "/qbFront/static/media/Bussiness StudiesBlue.8c53cd3d.png"
+  },
+  {
+    "revision": "addf957eaf32ae7ccab1a88374ef5a99",
+    "url": "/qbFront/static/media/Chemistry.addf957e.png"
+  },
+  {
+    "revision": "b06bf7481f32147ea056efc1eb52d6cb",
+    "url": "/qbFront/static/media/ChemistryBlue.b06bf748.png"
+  },
+  {
+    "revision": "356ca60826405f6135ae234757251bf6",
+    "url": "/qbFront/static/media/Economics.356ca608.png"
+  },
+  {
+    "revision": "70d45e797cda97b5243b14b19e89bbe5",
+    "url": "/qbFront/static/media/EconomicsBlue.70d45e79.png"
+  },
+  {
+    "revision": "d855ad2a604cc3b33199cbc49022bcdf",
+    "url": "/qbFront/static/media/EveryQuestion.d855ad2a.png"
+  },
+  {
+    "revision": "3cb3fa4e29e5ac26f00bb30cdfa44440",
+    "url": "/qbFront/static/media/EveryQuestionMobile.3cb3fa4e.png"
+  },
+  {
+    "revision": "e384b81110129aaccbcba675350b99a6",
+    "url": "/qbFront/static/media/ICSE.e384b811.png"
+  },
+  {
+    "revision": "d25c117edfad28aaf6dc8da07cfd24ab",
+    "url": "/qbFront/static/media/Mathematics.d25c117e.png"
+  },
+  {
+    "revision": "364d26501c0bcb3e415c68d6fa565dee",
+    "url": "/qbFront/static/media/MathematicsBlue.364d2650.png"
+  },
+  {
+    "revision": "602871432e9d11c6d19d4e17e5bf7f0c",
+    "url": "/qbFront/static/media/Physics.60287143.png"
+  },
+  {
+    "revision": "3b36084c63e7e2f4ad4ff2991bffb05c",
+    "url": "/qbFront/static/media/PhysicsBlue.3b36084c.png"
+  },
+  {
+    "revision": "e9cc99947828c78926db4d3d731905f8",
+    "url": "/qbFront/static/media/Science.e9cc9994.png"
+  },
+  {
+    "revision": "1458646b385f57efc4160314009798ba",
+    "url": "/qbFront/static/media/ScienceBlue.1458646b.png"
+  },
+  {
+    "revision": "de0a1601b506fd1fd2d6b0f6f8500438",
+    "url": "/qbFront/static/media/Social Science.de0a1601.png"
+  },
+  {
+    "revision": "0eafc60092e5d962dc921610978c1ac5",
+    "url": "/qbFront/static/media/Social ScienceBlue.0eafc600.png"
+  },
+  {
+    "revision": "32c4db8e4e213d3c0cfb13df8e5296e1",
+    "url": "/qbFront/static/media/laptop.32c4db8e.png"
+  },
+  {
+    "revision": "a1a27e3cf8e7c85af25e49d53ee21571",
+    "url": "/qbFront/static/media/profileIcon.a1a27e3c.png"
+  },
+  {
+    "revision": "95b8921c1fbc63b61de67cf07a8c3194",
+    "url": "/qbFront/static/media/progressClick.95b8921c.zip"
+  },
+  {
+    "revision": "a0724341baabc9332b1858cf5653c899",
+    "url": "/qbFront/static/media/registerImage.a0724341.png"
+  },
+  {
+    "revision": "230dbc53c5d078335fa75b94e5338960",
+    "url": "/qbFront/static/media/stuImageDemo.230dbc53.png"
+  },
+  {
+    "revision": "a4bda70ad24a4fcf2c0a26f3db9e5a29",
+    "url": "/qbFront/static/media/testimonial1.a4bda70a.png"
+  }
+]);
